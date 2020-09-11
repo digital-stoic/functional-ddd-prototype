cc0bee7396465d7bc506606058745619b3165365
mat <mat@matieux.net>
2020-09-11 20:50:54 +0800

    test(Api): Configuration DbConnectionString

---
+++ Tests/Api/Configuration.fs
@@ -0,0 +1,42 @@
+module Test.Api.Configuration
+
+open Expecto
+open Expecto.Flip.Expect
+open Test.Common.Helpers
+open Api
+
+//==============================================================================
+// Data
+//==============================================================================
+
+let appSettingsFilenameOK1 = "appsettings.json"
+
+let dbConnectionStringOK1 =
+    "AccountEndpoint=https://dummy.documents.azure.com:443/;AccountKey=dummy==;"
+
+//==============================================================================
+// Helpers
+//==============================================================================
+
+// TODO: Helper with record field name as parameter. See:
+//   - https://fpish.net/topic/None/57493
+//   - http://tomasp.net/blog/fsharp-dynamic-lookup.aspx/
+
+//==============================================================================
+// Tests
+//==============================================================================
+
+let testListWithoutDefault =
+    testList
+        "WithoutDefault"
+        [ test "DbConnectionString" {
+              let config =
+                  Configuration.build appSettingsFilenameOK1
+
+              config.DbConnectionString
+              |> equal "Config parsed" dbConnectionStringOK1
+          } ]
+
+let tests =
+    testList "Configuration"
+    <| testListAppend [ testListWithoutDefault ]
---
+++ Tests/Api/Main.fs
@@ -0,0 +1,10 @@
+﻿open Expecto
+open Test.Common.Helpers
+
+[<Tests>]
+let tests = 
+    testList "Api" <| testListAppend [ Test.Api.Configuration.tests ]
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssemblyWithCLIArgs [] argv
