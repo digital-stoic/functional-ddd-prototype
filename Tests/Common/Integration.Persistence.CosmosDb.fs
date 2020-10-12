b79d4f69d514a91cbeb5d3634cb02da2924b3432
mat <mat@matieux.net>
2020-10-12 19:51:47 +0800

    test(Common): Persistence CosmosDB. Tests disabled

--- Tests/Common/Integration.Persistence.CosmosDb.fs
+++ Tests/Common/Integration.Persistence.CosmosDb.fs
@@ -1,6 +1,6 @@
 module Test.Integration.Common.Infrastructure.Persistence.ComosDb
 
-
+open System
 open Expecto
 open Expecto.Flip.Expect
 open Test.Common.Helpers
@@ -10,29 +10,63 @@ open Common.Infrastructure.Persistence.ComosDb
 // Data
 //==============================================================================
 
+[<CLIMutable>]
+type ItemType =
+    { id: string
+      key: string
+      Question: string
+      Answer: int }
 
+let guidOK1 = Guid.NewGuid().ToString()
+
+let partitionKeyOK1 = "key1"
+
+let itemOK1 =
+    { id = guidOK1
+      key = partitionKeyOK1
+      Question = "Life, Universe and Everything"
+      Answer = 42 }
+
+let itemOK2 = { itemOK1 with Answer = 43 }
 
 //==============================================================================
 // Helpers
 //==============================================================================
 
 
-
 //==============================================================================
 // Tests
 //==============================================================================
 
-// let testListCosmosDb
-//     testList
-//         "WithoutDefault"
-//         [ test "DbConnectionString" {
-//               let config =
-//                   Configuration.build appSettingsFilenameOK1
+let tests database container connectionString =
+    testList
+        "PersistencesDb.CosmosDb"
+        [ testAsync "Insert an item" {
+              let! result =
+                  async {
+                      return insert<ItemType> database container connectionString itemOK1
+                             |> Async.RunSynchronously
+                  }
 
-//               config.DbConnectionString
-//               |> equal "Config parsed" dbConnectionStringOK1
-//           } ]
+              result |> equal "Item inserted" [ itemOK1 ]
+          }
 
-let tests =
-    testList "Common.Infrastructure.Persistence.CosmosDb"
-    <| testListAppend []
+          testAsync "Update an item by ID" {
+              printfn "ID: %A" guidOK1
+              let! result =
+                  async {
+                      //   return updateById<ItemType> database container connectionString guidOK1 partitionKeyOK1 itemOK2
+                      return updateById<ItemType>
+                                 database
+                                 container
+                                 connectionString
+                                 "af829c89-358b-434c-9260-b52b11c57622"
+                                 partitionKeyOK1
+                                 itemOK2
+                             |> Async.RunSynchronously
+                  }
+
+              result |> equal "Item inserted" [ itemOK2 ]
+          }
+
+          test "Dummy" { equal "dummy" 42 42 } ]
--- Tests/Common/Main.fs
+++ Tests/Common/Main.fs
@@ -7,17 +7,20 @@ let appSettingsFilenameTest = "appsettings-test.json"
 let appSettingsFilePathTest = Path.Combine(Directory.GetCurrentDirectory (), "..", "..", appSettingsFilenameTest)
 
 let config = Configuration.build appSettingsFilePathTest
+let db = config.Database
+let container = config.Container
+let connStr = config.DbConnectionString
 
 [<Tests>]
 let tests = 
     let testListUnit =
         testList "Unit" <|testListAppend  [ ]
     let testListIntegration =
-        testList "Integration" <|testListAppend  [ Test.Integration.Common.Infrastructure.Persistence.ComosDb.tests ]
+        // testList "Integration" <|testListAppend  [ Test.Integration.Common.Infrastructure.Persistence.ComosDb.tests db container connStr ]
+        testList "Integration" <|testListAppend  []
 
-    testList "Api" <| testListAppend [ testListUnit; testListIntegration ]
+    testList "Common" <| testListAppend [ testListUnit; testListIntegration ]
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" config.DbConnectionString
     Tests.runTestsInAssemblyWithCLIArgs [] argv
