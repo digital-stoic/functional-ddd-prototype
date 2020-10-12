fa4ec0f4d5bfb67a51d2f99db6e43cdd425d6934
mat <mat@matieux.net>
2020-10-12 19:51:20 +0800

    feat(Common): Persistence CosmosDB

--- Modules/Common/Infrastructure.Persistence.CosmosDb.fs
+++ Modules/Common/Infrastructure.Persistence.CosmosDb.fs
@@ -3,11 +3,22 @@ module Common.Infrastructure.Persistence.ComosDb
 open FSharp.Control
 open FSharp.CosmosDb
 
-let insert<'T> database container dbConnectionString data =
+let insert<'T> database container dbConnectionString item =
     dbConnectionString
     |> Cosmos.fromConnectionString
     |> Cosmos.database database
     |> Cosmos.container container
-    |> Cosmos.insert<'T> data
+    |> Cosmos.insert<'T> item
     |> Cosmos.execAsync<'T>
     |> AsyncSeq.toListAsync
+
+let updateById<'T> database container dbConnectionString id partitionKey newItem =
+    dbConnectionString
+    |> Cosmos.fromConnectionString
+    |> Cosmos.database database
+    |> Cosmos.container container
+    |> Cosmos.update<'T> id partitionKey (fun _ -> newItem)
+    |> Cosmos.execAsync<'T>
+    |> AsyncSeq.toListAsync
+
+// TODO: updateByCondition?
